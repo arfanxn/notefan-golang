19f5a436b2a650094c62bea67074528e4a7f0023
arfanxn <arfan2173@gmail.com>
2023-02-11 17:33:27 +0700

    update columns on page_content_change_history table

--- database/migrations/20230207230816_page_content_change_history.up.sql
+++ database/migrations/20230207230816_page_content_change_history.up.sql
@@ -1,8 +1,13 @@
-CREATE TABLE page_content_change_history (
-  id CHAR(36) PRIMARY KEY,  
-  type VARCHAR(50) NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP NULL
+CREATE TABLE `page_content_change_history` (
+  `before_page_content_id` CHAR(36),
+  `before_page_content_id` CHAR(36),
+  `user_id` CHAR(36),
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL,
+
+  FOREIGN KEY (before_page_content_id) REFERENCES page_contents(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (after_page_content_id) REFERENCES page_contents(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 ENGINE=InnoDB
 DEFAULT CHARACTER SET = utf8mb4
