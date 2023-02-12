9acbd8ca5751769078bcdacbf46477f111a34340
arfanxn <arfan2173@gmail.com>
2023-02-12 10:41:48 +0700

    update user factory and seeder

--- database/factories/user_factory.go
+++ database/factories/user_factory.go
@@ -8,20 +8,27 @@ import (
 
 	"github.com/go-faker/faker/v4"
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func NewUser() entities.User {
+	password, err := bcrypt.GenerateFromPassword([]byte("11112222"), bcrypt.DefaultCost)
+	helper.LogFatalIfError(err)
+
 	user := entities.User{
 		Id:        uuid.New(),
 		Name:      faker.Name(),
 		Email:     faker.Email(),
-		Password:  faker.Password(),
+		Password:  string(password),
 		CreatedAt: time.Now().AddDate(0, 0, -1),
 	}
 
 	updatedAt, ok := helper.Ternary(helper.BooleanRandom(), time.Now().AddDate(0, 0, -1), nil).(time.Time)
 	if ok {
-		user.UpdatedAt = sql.NullTime{Time: updatedAt}
+		user.UpdatedAt = sql.NullTime{
+			Time:  updatedAt,
+			Valid: true,
+		}
 	}
 
 	return user
--- database/seeders/user_seeder.go
+++ database/seeders/user_seeder.go
@@ -1,31 +1,61 @@
 package seeders
 
 import (
+	"database/sql"
 	"notefan-golang/database/factories"
 	"notefan-golang/helper"
+	"notefan-golang/models/entities"
 	"runtime"
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func UserSeeder(s *Seeder) {
+func UserSeeder(seeder DatabaseSeeder) {
 	// Consoler
 	pc, _, _, _ := runtime.Caller(0)
 	printStartRunning(pc)
 	defer printFinishRunning(pc)
 
-	// Begin
+	// ---- Begin ----
+	tableName := "users"
 	totalRows := 50
 	valueArgs := []any{}
+
 	for i := 0; i < totalRows; i++ {
+		if i == 0 {
+			password, err := bcrypt.GenerateFromPassword([]byte("11112222"), bcrypt.DefaultCost)
+			helper.PanicIfError(err)
+
+			user := entities.User{
+				Id:        uuid.New(),
+				Name:      "Muhammad Arfan",
+				Email:     "arf@gmail.com",
+				Password:  string(password),
+				CreatedAt: time.Now(),
+				UpdatedAt: sql.NullTime{
+					Time:  time.Now(),
+					Valid: true,
+				},
+			}
+			valueArgs = append(valueArgs,
+				user.Id.String(), user.Name, user.Email, user.Password, user.CreatedAt, user.UpdatedAt,
+			)
+
+			continue
+		}
+
 		user := factories.NewUser()
 		valueArgs = append(
 			valueArgs,
 			user.Id.String(), user.Name, user.Email, user.Password, user.CreatedAt, user.UpdatedAt)
 	}
 
-	query := helper.BuildBulkInsertQuery("users", totalRows,
+	query := helper.BuildBulkInsertQuery(tableName, totalRows,
 		`id`, `name`, `email`, `password`, `created_at`, `updated_at`)
 
-	stmt, err := s.db.Prepare(query)
+	stmt, err := seeder.db.Prepare(query)
 	helper.LogFatalIfError(err)
 
 	_, err = stmt.Exec(valueArgs...)
