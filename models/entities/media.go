b35e1e348a54bfdea6af0e4311d8b5869d1f873d
arfanxn <arfan2173@gmail.com>
2023-02-13 02:25:09 +0700

    update media model and repo

--- models/entities/media.go
+++ models/entities/media.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Media struct {
-	Id             string       `json:"id"`
+	Id             uuid.UUID    `json:"id"`
 	ModelType      string       `json:"model_type"`
 	ModelId        uuid.UUID    `json:"model_id"`
 	CollectionName string       `json:"collection_name"`
@@ -17,7 +17,7 @@ type Media struct {
 	MimeType       string       `json:"mime_type"`
 	Disk           string       `json:"disk"`
 	ConversionDisk string       `json:"conversion_disk"`
-	Size           int64        `json:"size"`
+	Size           int          `json:"size"`
 	CreatedAt      time.Time    `json:"created_at"`
 	UpdatedAt      sql.NullTime `json:"updated_at"`
 }
--- repositories/media_repo.go
+++ repositories/media_repo.go
@@ -1,15 +1,114 @@
 package repositories
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"notefan-golang/exceptions"
+	"notefan-golang/helper"
+	"notefan-golang/models/entities"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type MediaRepo struct {
-	tableName string
-	db        *sql.DB
+	db          *sql.DB
+	tableName   string
+	columnNames []string
 }
 
 func NewMediaRepo(db *sql.DB) *MediaRepo {
 	return &MediaRepo{
-		tableName: "medias",
-		db:        db,
+		db:          db,
+		tableName:   "medias",
+		columnNames: helper.GetStructFieldJsonTag(entities.Media{}),
 	}
+}
+
+func (repo *MediaRepo) All(ctx context.Context) ([]entities.Media, error) {
+	query := "SELECT " + helper.SliceTableColumnsToString(repo.columnNames) + " FROM " + repo.tableName
+	medias := []entities.Media{}
+	rows, err := repo.db.QueryContext(ctx, query)
+	if err != nil {
+		helper.LogIfError(err)
+		return medias, err
+	}
+
+	for rows.Next() {
+		media := entities.Media{}
+		err := rows.Scan(
+			&media.Id,
+			&media.ModelType,
+			&media.ModelId,
+			&media.CollectionName,
+			&media.Name,
+			&media.FileName,
+			&media.MimeType,
+			&media.Disk,
+			&media.ConversionDisk,
+			&media.Size,
+			&media.CreatedAt,
+			&media.UpdatedAt,
+		)
+		if err != nil {
+			helper.LogIfError(err)
+			return medias, err
+		}
+		medias = append(medias, media)
+	}
+
+	if len(medias) == 0 {
+		return medias, exceptions.DataNotFoundError
+	}
+
+	return medias, nil
+}
+
+func (repo *MediaRepo) Insert(ctx context.Context, medias ...entities.Media) ([]entities.Media, error) {
+	query := buildBatchInsertQuery(repo.tableName, len(medias), repo.columnNames...)
+	valueArgs := []any{}
+
+	for _, media := range medias {
+		if media.Id.String() == "" {
+			media.Id = uuid.New()
+		}
+		if media.CreatedAt.IsZero() {
+			media.CreatedAt = time.Now()
+		}
+		valueArgs = append(valueArgs,
+			media.Id,
+			media.ModelType,
+			media.ModelId,
+			media.CollectionName,
+			media.Name,
+			media.FileName,
+			media.MimeType,
+			media.Disk,
+			media.ConversionDisk,
+			media.Size,
+			media.CreatedAt,
+			media.UpdatedAt,
+		)
+	}
+
+	stmt, err := repo.db.PrepareContext(ctx, query)
+	if err != nil {
+		helper.LogIfError(err)
+		return medias, err
+	}
+	_, err = stmt.ExecContext(ctx, valueArgs...)
+	if err != nil {
+		helper.LogIfError(err)
+		return medias, err
+	}
+	return medias, nil
+}
+
+func (repo *MediaRepo) Create(ctx context.Context, media entities.Media) (entities.Media, error) {
+	medias, err := repo.Insert(ctx, media)
+	if err != nil {
+		return entities.Media{}, err
+	}
+
+	return medias[0], nil
 }
