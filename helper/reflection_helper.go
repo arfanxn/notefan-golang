8bc7070fe8b8e94f0ec3341b1008dc0bdcd84138
arfanxn <arfan2173@gmail.com>
2023-02-13 00:56:50 +0700

    add reflection helper

--- helper/reflection_helper.go
+++ helper/reflection_helper.go
@@ -16,3 +16,11 @@ func GetStructFieldJsonTag(structure any) []string {
 
 	return tags
 }
+
+func GetTypeName(myvar interface{}) string {
+	if t := reflect.TypeOf(myvar); t.Kind() == reflect.Ptr {
+		return "*" + t.Elem().Name()
+	} else {
+		return t.Name()
+	}
+}
