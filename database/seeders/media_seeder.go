76fac0663187a57cd6157483230b3fb6f8b7b9fb
arfanxn <arfan2173@gmail.com>
2023-02-13 02:25:21 +0700

    make uncompleted media seeder

---
+++ database/seeders/media_seeder.go
@@ -0,0 +1,38 @@
+package seeders
+
+import (
+	"context"
+	"database/sql"
+	"notefan-golang/repositories"
+	"runtime"
+	"time"
+)
+
+type MediaSeeder struct {
+	db        *sql.DB
+	tableName string
+	repo      *repositories.MediaRepo
+}
+
+func NewMediaSeeder(db *sql.DB) *MediaSeeder {
+	return &MediaSeeder{
+		db:        db,
+		tableName: "medias",
+		repo:      repositories.NewMediaRepo(db),
+	}
+}
+
+func (seeder *MediaSeeder) Run() {
+	// Consoler
+	pc, _, _, _ := runtime.Caller(0)
+	printStartRunning(pc)
+	defer printFinishRunning(pc)
+
+	// ---- Begin ----
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute/2) // Give a 30 second timeout
+	defer cancel()
+
+	_ = ctx
+
+	// TODO : Complete media seeder
+}
