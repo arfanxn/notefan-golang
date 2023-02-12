3a687b12043de815c151925cfc44f2cbaf41145d
arfanxn <arfan2173@gmail.com>
2023-02-12 16:26:09 +0700

    fix typo

--- models/entities/page.go
+++ models/entities/page.go
@@ -10,7 +10,7 @@ import (
 type Page struct {
 	Id        uuid.UUID    `json:"id"`
 	SpaceId   uuid.UUID    `json:"space_id"`
-	Title     string       `json:"name"`
+	Title     string       `json:"title"`
 	Order     int          `json:"order"`
 	CreatedAt time.Time    `json:"created_at"`
 	UpdatedAt sql.NullTime `json:"updated_at"`
