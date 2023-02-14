182f84ed2ff1dbc02d6be083c5d38eb6706a673f
arfanxn <arfan2173@gmail.com>
2023-02-14 23:46:43 +0700

    update error helprs argument type

--- helper/error_helper.go
+++ helper/error_helper.go
@@ -12,32 +12,32 @@ func ErrorPanic(err error) {
 	}
 }
 
-func ErrorLog(err error) {
+func ErrorLog(err any) {
 	if err != nil {
 		logger := logrus.New()
 		file, openFileError := os.OpenFile("logs/application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 		ErrorPanic(openFileError)
 		logger.SetOutput(file)
-		logger.WithError(err).Error() // error will continue the program
+		logger.Error(err) // error will continue the program
 	}
 }
 
-func ErrorLogFatal(err error) {
+func ErrorLogFatal(err any) {
 	if err != nil {
 		logger := logrus.New()
 		file, openFileError := os.OpenFile("logs/application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 		ErrorLogPanic(openFileError)
 		logger.SetOutput(file)
-		logger.WithError(err).Fatal() // fatal will exit the program immediately
+		logger.Fatal(err) // fatal will exit the program immediately
 	}
 }
 
-func ErrorLogPanic(err error) {
+func ErrorLogPanic(err any) {
 	if err != nil {
 		logger := logrus.New()
 		file, openFileError := os.OpenFile("logs/application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 		ErrorPanic(openFileError)
 		logger.SetOutput(file)
-		logger.WithError(err).Panic() // panic will panicing the program immediately
+		logger.Panic(err) // panic will panicing the program immediately
 	}
 }
