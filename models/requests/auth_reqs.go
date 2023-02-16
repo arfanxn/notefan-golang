b4f251f7a50600ba895ab229bb94326887cd202e
arfanxn <arfan2173@gmail.com>
2023-02-17 01:32:41 +0700

    update validation rule

--- models/requests/auth_reqs.go
+++ models/requests/auth_reqs.go
@@ -2,7 +2,7 @@ package requests
 
 type AuthLogin struct {
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required"`
+	Password string `json:"password" validate:"required,min=2,max=50"`
 }
 
 type AuthRegister struct {
