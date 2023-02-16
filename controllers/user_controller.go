11e9a391c9da666f9d070b24cb5ec595a7518db7
arfanxn <arfan2173@gmail.com>
2023-02-16 13:38:50 +0700

    add method to usercontroller

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/notefan-golang/helper"
+	"github.com/notefan-golang/models/responses"
 	"github.com/notefan-golang/services"
 )
 
@@ -14,6 +16,21 @@ func NewUserController(service *services.UserService) *UserController {
 	return &UserController{service: service}
 }
 
-func (controller UserController) Something(w http.ResponseWriter, r *http.Request) {
-	// TODO
+// Self gets the current logged in user
+func (controller UserController) Self(w http.ResponseWriter, r *http.Request) {
+	userId := r.Context().Value("user").(map[string]any)["id"].(string)
+	user, err := controller.service.Repository.Find(r.Context(), userId)
+	helper.ErrorPanic(err)
+
+	response := responses.NewResponse().
+		Code(http.StatusOK).
+		Success("Successfully retrived current user").
+		Body("user", responses.User{
+			Id:        user.Id.String(),
+			Name:      user.Name,
+			Email:     user.Email,
+			CreatedAt: user.CreatedAt,
+			UpdatedAt: user.UpdatedAt.Time,
+		})
+	helper.ResponseJSON(w, response)
 }
