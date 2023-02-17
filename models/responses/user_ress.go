8df8fe6ec59699006749167e66f13c43dc843c1c
arfanxn <arfan2173@gmail.com>
2023-02-18 03:17:32 +0700

    update user ress

--- models/responses/user_ress.go
+++ models/responses/user_ress.go
@@ -2,6 +2,9 @@ package responses
 
 import (
 	"time"
+
+	"github.com/notefan-golang/models/entities"
+	"gopkg.in/guregu/null.v4"
 )
 
 type User struct {
@@ -9,5 +12,17 @@ type User struct {
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	UpdatedAt null.Time `json:"updated_at"`
+
+	Avatar Media `json:"avatar,omitempty"`
+}
+
+func NewUserFromEntity(entity entities.User) User {
+	return User{
+		Id:        entity.Id.String(),
+		Name:      entity.Name,
+		Email:     entity.Email,
+		CreatedAt: entity.CreatedAt,
+		UpdatedAt: null.NewTime(entity.UpdatedAt.Time, entity.UpdatedAt.Valid),
+	}
 }
