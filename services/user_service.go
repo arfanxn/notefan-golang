5658c9fbecf84afd2edba5eb57c606b22dd6b142
arfanxn <arfan2173@gmail.com>
2023-02-18 03:20:05 +0700

    add find method on user service  (not complete)

--- services/user_service.go
+++ services/user_service.go
@@ -3,17 +3,47 @@ package services
 import (
 	"context"
 
+	"github.com/notefan-golang/exceptions"
+	"github.com/notefan-golang/helper"
+	"github.com/notefan-golang/models/responses"
 	"github.com/notefan-golang/repositories"
 )
 
 type UserService struct {
-	Repository *repositories.UserRepository
+	repository      *repositories.UserRepository
+	mediaRepository *repositories.MediaRepository
 }
 
-func NewUserService(repository *repositories.UserRepository) *UserService {
-	return &UserService{Repository: repository}
+func NewUserService(
+	repository *repositories.UserRepository,
+	mediaRepository *repositories.MediaRepository,
+) *UserService {
+	return &UserService{repository: repository, mediaRepository: mediaRepository}
 }
 
-func (service *UserService) Something(ctx context.Context) {
-	// TODO
+// Find Finds user by id and returns user with its relations
+func (service *UserService) Find(ctx context.Context, id string) (responses.User, error) {
+	user := responses.User{}
+
+	userEntity, err := service.repository.Find(ctx, id)
+	if err != nil { // err not nil return exception HTTPNotFound
+		helper.ErrorLog(err)
+		return user, exceptions.HTTPNotFound
+	}
+	user = responses.NewUserFromEntity(userEntity)
+
+	// TODO : load user with avatar (profile_picture)
+	// // User avatar
+	// avatar, err := service.mediaRepository.FindByModelAndCollectionName(
+	// 	ctx,
+	// 	helper.ReflectGetTypeName(userEntity),
+	// 	userEntity.Id.String(),
+	// 	"avatar",
+	// )
+	// if errors.Is(err, exceptions.HTTPNotFound) { // if user doesn't have avatar return user without avatar
+	// 	return user, nil
+	// }
+
+	// user.Avatar = responses.NewMediaFromEntity(avatar) // assign user avatar with avatar
+	return user, nil
 }
