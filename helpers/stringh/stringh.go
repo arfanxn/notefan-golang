c7c75aa77fc90340c4a2957fc59dd179fc3078e0
arfanxn <arfan2173@gmail.com>
2023-03-02 01:50:54 +0700

    add funcs to stringh

--- helpers/stringh/stringh.go
+++ helpers/stringh/stringh.go
@@ -1,6 +1,9 @@
 package stringh
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 // SliceColumnToStr format a slice of database table column names to a string
 // return example "`column1`, `column2`, `column3`"
@@ -10,4 +13,18 @@ func SliceColumnToStr(columns []string) string {
 		names = append(names, "`"+columns[i]+"`")
 	}
 	return strings.Join(names, ", ")
+}
+
+func SnakeCaseToCapitalized(str string) string {
+	str = strings.ReplaceAll(str, "_", " ")
+	str = strings.TrimSpace(str)
+	firstCharOfStr := []rune(str)[0]
+	str = ReplaceAtIndex(str, 0, unicode.ToUpper(firstCharOfStr))
+	return str
+}
+
+func ReplaceAtIndex(in string, i int, r rune) string {
+	out := []rune(in)
+	out[i] = r
+	return string(out)
 }
