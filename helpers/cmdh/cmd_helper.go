fffaa378fc2159d382b658d777aeb9d9776457da
arfanxn <arfan2173@gmail.com>
2023-03-02 00:46:47 +0700

    remove helpers

--- helpers/cmdh/cmd_helper.go
+++ helpers/cmdh/cmd_helper.go
@@ -1,11 +1,11 @@
-package helper
+package cmdh
 
 import (
 	"os"
 	"strings"
 )
 
-func CMDUserFirstArgIs(expetedArg string) bool {
+func UserFirstArgIs(expetedArg string) bool {
 	if len(os.Args) <= 1 { // return false if only one argument or less (one argument is default provided by Go-Lang, so it's not considered as first argument from user input)
 		return false
 	}
