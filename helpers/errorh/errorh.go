20f074b5d2f7e7d71b195003a74022109730c41c
arfanxn <arfan2173@gmail.com>
2023-03-02 01:21:35 +0700

    refactor error helper

---
+++ helpers/errorh/errorh.go
@@ -0,0 +1,52 @@
+package errorh
+
+import (
+	"os"
+
+	"github.com/notefan-golang/exceptions"
+
+	"github.com/sirupsen/logrus"
+)
+
+func Panic(anyErr any) {
+	if anyErr != nil {
+		err, ok := anyErr.(error)
+
+		httpErr, ok := err.(*exceptions.HTTPError)
+		if ok {
+			panic(httpErr)
+		}
+
+		panic(err)
+	}
+}
+
+func Log(err any) {
+	if err != nil {
+		logger := logrus.New()
+		file, openFileError := os.OpenFile("logs/application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		Panic(openFileError)
+		logger.SetOutput(file)
+		logger.Error(err) // error will continue the program
+	}
+}
+
+func LogFatal(err any) {
+	if err != nil {
+		logger := logrus.New()
+		file, openFileError := os.OpenFile("logs/application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		Panic(openFileError)
+		logger.SetOutput(file)
+		logger.Fatal(err) // fatal will exit the program immediately
+	}
+}
+
+func LogPanic(err any) {
+	if err != nil {
+		logger := logrus.New()
+		file, openFileError := os.OpenFile("logs/application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+		Panic(openFileError)
+		logger.SetOutput(file)
+		logger.Panic(err) // panic will panicing the program immediately
+	}
+}
