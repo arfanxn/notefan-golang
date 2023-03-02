e082910a023ecb619f25a1a3902312170e6d0a13
arfanxn <arfan2173@gmail.com>
2023-03-02 18:19:07 +0700

    add validation excpts

--- exceptions/validation_exceptions.go
+++ exceptions/validation_exceptions.go
@@ -5,4 +5,10 @@ import "errors"
 var (
 	// ValidationPasswordNotMatch
 	ValidationPasswordNotMatch error = errors.New("Password not match")
+
+	// ValidationFileSize
+	ValidationFileSize error = errors.New("File size is too large or too small")
+
+	// ValidationFileMimeType
+	ValidationFileMimeType error = errors.New("File mimetype is not supported")
 )
