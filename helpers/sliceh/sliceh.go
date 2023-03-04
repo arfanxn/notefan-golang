6caa4dd1e11899e9180ed252cd0ae2f2be84dd5f
arfanxn <arfan2173@gmail.com>
2023-03-05 01:45:47 +0700

    add helper funct

--- helpers/sliceh/sliceh.go
+++ helpers/sliceh/sliceh.go
@@ -1,5 +1,7 @@
 package sliceh
 
+import "math/rand"
+
 func Chunk[T any](items []T, size int) (chunks [][]T) {
 	for size < len(items) {
 		items, chunks = items[size:], append(chunks, items[0:size:size])
@@ -15,4 +17,12 @@ func Filter[T any](items []T, callback func(T) bool) []T {
 		}
 	}
 	return matchItems
+}
+
+func Random[T any](slice []T) T {
+	if len(slice) > 1 {
+		return slice[rand.Intn(len(slice)-1)]
+	} else {
+		return slice[0]
+	}
 }
