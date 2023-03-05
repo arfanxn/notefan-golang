dbaca0f209229fc8093f59bb1c3a12dc22f5089b
arfanxn <arfan2173@gmail.com>
2023-03-06 02:22:43 +0700

    refactor user repo test

--- tests/units/repositories/user_repository_test.go
+++ tests/units/repositories/user_repository_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/go-faker/faker/v4"
 	"github.com/google/uuid"
 	"github.com/notefan-golang/containers/singletons"
 	"github.com/notefan-golang/database/factories"
@@ -20,103 +21,67 @@ func TestUserRepository(t *testing.T) {
 	userRepository := repositories.NewUserRepository(app.DB)
 	ctx := context.Background()
 
-	t.Run("Insert", func(t *testing.T) {
-		t.Run("Success", func(t *testing.T) {
-			expectedUsers := []entities.User{
-				factories.FakeUser(),
-				factories.FakeUser(),
-			}
-			for i := range expectedUsers {
-				expectedUser := &expectedUsers[i]
-				expectedUser.UpdatedAt.Time = time.Time{} // Assign zero time
-				expectedUser.UpdatedAt.Valid = false      // invalidate updated_at timestamp
-			}
-			result, err := userRepository.Insert(ctx, &expectedUsers[0], &expectedUsers[1])
-			require.Nil(err)
-			require.NotZero(result.RowsAffected())
-
-			for _, expectedUser := range expectedUsers {
-				require.NotEqual(uuid.Nil, expectedUser.Id)
-				require.NotZero(expectedUser.CreatedAt)
-			}
-		})
-	})
+	var user entities.User
 
 	t.Run("Create", func(t *testing.T) {
-		t.Run("Success", func(t *testing.T) {
-			expectedUser := factories.FakeUser()
-			expectedUser.UpdatedAt.Time = time.Time{} // Assign zero time
-			expectedUser.UpdatedAt.Valid = false      // invalidate updated_at timestamp
+		user = factories.FakeUser()
+		user.UpdatedAt.Time = time.Time{} // Assign zero time
+		user.UpdatedAt.Valid = false      // invalidate updated_at timestamp
 
-			result, err := userRepository.Create(ctx, &expectedUser)
-			require.Nil(err)
-			require.NotZero(result.RowsAffected())
+		result, err := userRepository.Create(ctx, &user)
+		require.Nil(err)
+		require.NotZero(result.RowsAffected())
 
-			require.NotEqual(uuid.Nil, expectedUser.Id)
-			require.NotZero(expectedUser.CreatedAt)
-		})
+		require.NotEqual(uuid.Nil, user.Id)
+		require.NotZero(user.CreatedAt)
 	})
 
 	t.Run("UpdateById", func(t *testing.T) {
-		t.Run("Success", func(t *testing.T) {
-			actualUser := factories.FakeUser()
-			actualUser.UpdatedAt.Time = time.Time{} // Assign zero time
-			actualUser.UpdatedAt.Valid = false      // invalidate updated_at timestamp
-			_, err := userRepository.Create(ctx, &actualUser)
-			require.Nil(err)
-			require.NotEqual(actualUser.Id, uuid.Nil)
+		actualUser := user
+		expectedUser := actualUser
+		expectedUser.Name = faker.Name()
+		expectedUser.Email = faker.Email()
 
-			expectedUser := actualUser
-			expectedUser.Name = "Arfan"
-			expectedUser.Email = "arf@gm.com"
+		require.Equal(expectedUser.Id, actualUser.Id)
 
-			require.Equal(expectedUser.Id, actualUser.Id)
+		result, err := userRepository.UpdateById(ctx, &expectedUser)
+		require.Nil(err)
+		require.NotZero(result.RowsAffected())
 
-			result, err := userRepository.UpdateById(ctx, &expectedUser)
-			require.Nil(err)
-			require.NotZero(result.RowsAffected())
+		require.NotZero(expectedUser.UpdatedAt.Time)
 
-			require.NotZero(expectedUser.UpdatedAt.Time)
-		})
+		user = expectedUser
 	})
 
 	t.Run("All", func(t *testing.T) {
-		t.Run("Success", func(t *testing.T) {
-			users, err := userRepository.All(ctx)
-			require.Nil(err)
-			require.NotEmpty(users)
-		})
+		users, err := userRepository.All(ctx)
+		require.Nil(err)
+		require.NotEmpty(users)
 	})
 
 	t.Run("FindById", func(t *testing.T) {
-		t.Run("Success", func(t *testing.T) {
-			users, err := userRepository.All(ctx)
-			require.Nil(err)
-			actualUser := users[len(users)-1]
-			expectedUser, err := userRepository.Find(ctx, actualUser.Id.String())
-			require.Nil(err)
-			require.Equal(expectedUser.Id.String(), actualUser.Id.String())
-			require.Equal(expectedUser.Name, actualUser.Name)
-			require.Equal(expectedUser.Email, actualUser.Email)
-			require.NotEmpty(actualUser.Password)
-			require.NotZero(actualUser.CreatedAt)
-			require.Zero(actualUser.UpdatedAt)
-		})
+		actualUser := user
+		expectedUser, err := userRepository.Find(ctx, actualUser.Id.String())
+		require.Nil(err)
+		require.Equal(expectedUser.Id.String(), actualUser.Id.String())
+		require.Equal(expectedUser.Name, actualUser.Name)
+		require.Equal(expectedUser.Email, actualUser.Email)
+		require.NotEmpty(actualUser.Password)
+		require.NotZero(actualUser.CreatedAt)
+
+		user = expectedUser
 	})
 
 	t.Run("FindByEmail", func(t *testing.T) {
-		t.Run("Success", func(t *testing.T) {
-			users, err := userRepository.All(ctx)
-			require.Nil(err)
-			actualUser := users[len(users)-1]
-			expectedUser, err := userRepository.FindByEmail(ctx, actualUser.Email)
-			require.Nil(err)
-			require.Equal(expectedUser.Id.String(), actualUser.Id.String())
-			require.Equal(expectedUser.Name, actualUser.Name)
-			require.Equal(expectedUser.Email, actualUser.Email)
-			require.NotEmpty(actualUser.Password)
-			require.NotZero(actualUser.CreatedAt)
-			require.Zero(actualUser.UpdatedAt)
-		})
+		actualUser := user
+		expectedUser, err := userRepository.FindByEmail(ctx, actualUser.Email)
+		require.Nil(err)
+		require.Equal(expectedUser.Id.String(), actualUser.Id.String())
+		require.Equal(expectedUser.Name, actualUser.Name)
+		require.Equal(expectedUser.Email, actualUser.Email)
+		require.NotEmpty(actualUser.Password)
+		require.NotZero(actualUser.CreatedAt)
+
+		user = expectedUser
 	})
 }
