d9e78d11e2b522e12b6855c9793001084920f3de
arfanxn <arfan2173@gmail.com>
2023-03-06 12:01:13 +0700

    make media route

--- routes/media_routes.go
+++ routes/media_routes.go
@@ -1,1 +1,19 @@
 package routes
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	cc "github.com/notefan-golang/containers/controllers"
+)
+
+// registerMediaRoutes registers routes for user module
+func registerMediaRoutes(router *mux.Router, db *sql.DB) {
+	mediaController := cc.InitializeMediaController(db)
+
+	// Media subrouters
+	medias := router.PathPrefix("/medias").Subrouter()
+
+	medias.HandleFunc("id", mediaController.Find).Methods(http.MethodGet)
+}
