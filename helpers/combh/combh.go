15f0da4c9ca9dbf18dd2dc9d8504499c897eb128
arfanxn <arfan2173@gmail.com>
2023-03-07 12:31:11 +0700

    make combination helper functioon

--- helpers/combh/combh.go
+++ helpers/combh/combh.go
@@ -2,24 +2,31 @@
 package combh
 
 import (
-	"io"
+	"net/url"
 
+	"github.com/gorilla/schema"
 	"github.com/notefan-golang/helpers/errorh"
-	"github.com/notefan-golang/helpers/jsonh"
 	"github.com/notefan-golang/helpers/validationh"
 	reqContracts "github.com/notefan-golang/models/requests/req_contracts"
 )
 
-// RequestBodyDecodeValidate will decode the request body and then validate the decoded request body
-// then return decoded request body and error if it fails
-func RequestBodyDecodeValidate[T reqContracts.ValidateableContract](requestBody io.ReadCloser) (
+// FormDataDecodeValidate will decode the form data and then validate the decoded form data
+// then return decoded form data and error if it fails
+func FormDataDecodeValidate[T reqContracts.ValidateableContract](
+	formData url.Values,
+) (
 	T, error) {
+	var input T
+
 	// Decode the request body
-	input, err := jsonh.DecodeFromReader[T](requestBody)
+	err := schema.NewDecoder().Decode(&input, formData)
 	errorh.LogPanic(err)
 
 	// Validate the input
 	err = validationh.ValidateStruct(input)
+	if err != nil { // if validation fails then return the valiudation error
+		return input, err
+	}
 
-	return input, err
+	return input, nil
 }
