bf967aa9e274cf5530e45a196ec24aa95cb0e225
arfanxn <arfan2173@gmail.com>
2023-03-07 12:54:43 +0700

    refactor login controller

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -6,7 +6,7 @@ import (
 	"github.com/notefan-golang/helpers/combh"
 	"github.com/notefan-golang/helpers/errorh"
 	"github.com/notefan-golang/helpers/rwh"
-	authReqs "github.com/notefan-golang/models/requests/auth_reqs"
+	"github.com/notefan-golang/models/requests/auth_reqs"
 	"github.com/notefan-golang/models/responses"
 	"github.com/notefan-golang/services"
 )
@@ -19,8 +19,9 @@ func NewAuthController(service *services.AuthService) *AuthController {
 	return &AuthController{service: service}
 }
 
+// Login will signing a user with the given credentials
 func (controller AuthController) Login(w http.ResponseWriter, r *http.Request) {
-	input, err := combh.RequestBodyDecodeValidate[authReqs.Login](r.Body)
+	input, err := combh.FormDataDecodeValidate[auth_reqs.Login](r.Form)
 	errorh.Panic(err)
 
 	authLoginRes, err := controller.service.Login(r.Context(), input)
@@ -43,6 +44,7 @@ func (controller AuthController) Login(w http.ResponseWriter, r *http.Request) {
 	)
 }
 
+// Logout will signing out the signed in if user
 func (controller AuthController) Logout(w http.ResponseWriter, r *http.Request) {
 	// Delete token from cookie
 	http.SetCookie(w, &http.Cookie{
@@ -55,17 +57,19 @@ func (controller AuthController) Logout(w http.ResponseWriter, r *http.Request) 
 	rwh.WriteResponse(w, responses.NewResponse().Code(http.StatusOK).Success("Logout successfully"))
 }
 
+// Register registers a new user with the given username and password and other parameters
 func (controller AuthController) Register(w http.ResponseWriter, r *http.Request) {
-	input, err := combh.RequestBodyDecodeValidate[authReqs.Register](r.Body)
+	input, err := combh.FormDataDecodeValidate[auth_reqs.Register](r.Form)
 	errorh.Panic(err)
 
 	// Register the user
 	userRes, err := controller.service.Register(r.Context(), input)
 	errorh.LogPanic(err)
 
-	// Send response and the registered user
+	// Send response with registered user data
 	rwh.WriteResponse(w, responses.NewResponse().
 		Code(http.StatusCreated).
 		Success("Successfully registered").
 		Body("user", userRes))
+
 }
