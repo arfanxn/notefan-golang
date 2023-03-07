6b8c177fda6900830614e00b7610351505f8e21e
arfanxn <arfan2173@gmail.com>
2023-03-07 12:39:54 +0700

    make decode helper

---
+++ helpers/decodeh/decodeh.go
@@ -0,0 +1,17 @@
+package decodeh
+
+import (
+	"net/url"
+
+	"github.com/gorilla/schema"
+	"github.com/notefan-golang/helpers/errorh"
+	requests "github.com/notefan-golang/models/requests/req_contracts"
+)
+
+// FormData decodes form data, return decoded data or panic if error happens
+func FormData[T requests.ValidateableContract](formData url.Values) (input T) {
+	// Decode the request body
+	err := schema.NewDecoder().Decode(&input, formData)
+	errorh.LogPanic(err)
+	return input
+}
