61aa997877f056e60e31dd5fac6b9892835fa5ed
arfanxn <arfan2173@gmail.com>
2023-03-07 12:42:01 +0700

    refactor combination helper

--- helpers/combh/combh.go
+++ helpers/combh/combh.go
@@ -4,26 +4,21 @@ package combh
 import (
 	"net/url"
 
-	"github.com/gorilla/schema"
-	"github.com/notefan-golang/helpers/errorh"
+	"github.com/notefan-golang/helpers/decodeh"
 	"github.com/notefan-golang/helpers/validationh"
 	reqContracts "github.com/notefan-golang/models/requests/req_contracts"
 )
 
 // FormDataDecodeValidate will decode the form data and then validate the decoded form data
-// then return decoded form data and error if it fails
+// then return decoded form data and error if the validation fails
 func FormDataDecodeValidate[T reqContracts.ValidateableContract](
 	formData url.Values,
 ) (
 	T, error) {
-	var input T
-
-	// Decode the request body
-	err := schema.NewDecoder().Decode(&input, formData)
-	errorh.LogPanic(err)
+	input := decodeh.FormData[T](formData)
 
 	// Validate the input
-	err = validationh.ValidateStruct(input)
+	err := validationh.ValidateStruct(input)
 	if err != nil { // if validation fails then return the valiudation error
 		return input, err
 	}
