50bbd72c48d7fb97261f4ac051da1683c74dd7ac
arfanxn <arfan2173@gmail.com>
2023-03-08 21:43:02 +0700

    add query builder

--- repositories/repository.go
+++ repositories/repository.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+// buildBatchInsertQuery builds a batch insert query
 func buildBatchInsertQuery(tableName string, totalRows int, columnNames ...string) string {
 	replaceable := "{COLUMN_NAMES}"
 	query := "INSERT INTO " + tableName + "(" + replaceable + ") VALUES "
@@ -27,6 +28,7 @@ func buildBatchInsertQuery(tableName string, totalRows int, columnNames ...strin
 	return query + strings.Join(valueStrs, ", ")
 }
 
+// buildUpdateQuery builds an update query
 func buildUpdateQuery(tableName string, columnNames ...string) string {
 	queryBuf := bytes.NewBufferString("UPDATE ")
 	queryBuf.WriteString(tableName)
@@ -39,4 +41,20 @@ func buildUpdateQuery(tableName string, columnNames ...string) string {
 	queryBuf.WriteString(strings.Join(formattedSetables, ", "))
 
 	return queryBuf.String()
+}
+
+// buildBatchDeleteQueryByIds builds a batch delete query by given ids
+func buildBatchDeleteQueryByIds(tableName string, ids ...string) (string, []any) {
+	var valueArgs []any
+	queryBuf := bytes.NewBufferString("DELETE FROM ")
+	queryBuf.WriteString(tableName)
+	queryBuf.WriteString(" WHERE ")
+	for index, id := range ids {
+		valueArgs = append(valueArgs, id)
+		queryBuf.WriteString("`id` = ?")
+		if (index + 1) != len(ids) { // if current is not last index
+			queryBuf.WriteString(" OR ")
+		}
+	}
+	return queryBuf.String(), valueArgs
 }
