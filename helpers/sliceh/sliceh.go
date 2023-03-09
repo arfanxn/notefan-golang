673711f4c1c55b6687af754ea43c5888e5e891f1
arfanxn <arfan2173@gmail.com>
2023-03-09 09:30:08 +0700

    make Map helper funtion

--- helpers/sliceh/sliceh.go
+++ helpers/sliceh/sliceh.go
@@ -2,6 +2,7 @@ package sliceh
 
 import "math/rand"
 
+// Chunk will separate an array by the given number of size
 func Chunk[T any](items []T, size int) (chunks [][]T) {
 	for size < len(items) {
 		items, chunks = items[size:], append(chunks, items[0:size:size])
@@ -9,6 +10,7 @@ func Chunk[T any](items []T, size int) (chunks [][]T) {
 	return append(chunks, items)
 }
 
+// Filter returns only items that satisfy the given predicate (return the true predicate condition only)
 func Filter[T any](items []T, callback func(T) bool) []T {
 	matchItems := []T{}
 	for _, item := range items {
@@ -19,6 +21,16 @@ func Filter[T any](items []T, callback func(T) bool) []T {
 	return matchItems
 }
 
+// Map mapping slice of T
+func Map[T any](items []T, callback func(T) T) []T {
+	var resultItems []T
+	for _, item := range items {
+		resultItems = append(resultItems, callback(item))
+	}
+	return resultItems
+}
+
+// Random return a random T from the given slice of T
 func Random[T any](slice []T) T {
 	if len(slice) > 1 {
 		return slice[rand.Intn(len(slice)-1)]
