c370d35ecd39dce09cd2fc7dd7151b343a727563
arfanxn <arfan2173@gmail.com>
2023-03-09 19:42:59 +0700

    remove param

--- helpers/nullh/nullh.go
+++ helpers/nullh/nullh.go
@@ -1,12 +1,10 @@
 package nullh
 
 import (
-	"time"
-
 	"gopkg.in/guregu/null.v4"
 )
 
 // NullInt returns an null.Int with null value
-func NullInt(datetime time.Time) null.Int {
+func NullInt() null.Int {
 	return null.NewInt(0, false)
 }
