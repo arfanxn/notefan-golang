e982f4c7390fb0659e587d46d1a272562158bd82
arfanxn <arfan2173@gmail.com>
2023-03-09 16:31:51 +0700

    update request validation

--- models/requests/space_reqs/get_by_user.go
+++ models/requests/space_reqs/get_by_user.go
@@ -6,23 +6,17 @@ import (
 
 type GetByUser struct {
 	UserId  string `json:"user_id"`
-	Offset  int    `json:"offset"`
-	PerPage int    `json:"page"`
-	Limit   int    `json:"limit"`
+	Page    int    `json:"page"`
+	PerPage int    `json:"per_page"`
 	OrderBy string `json:"order_by"`
 }
 
 // Validate validates the request
 func (input GetByUser) Validate() error {
 	return validation.ValidateStruct(&input,
-		validation.Field(&input.UserId,
-			validation.Required),
-		validation.Field(&input.Offset,
-			validation.When(input.PerPage == 0, validation.Required)),
-		validation.Field(&input.PerPage,
-			validation.When(input.Offset == 0, validation.Required)),
-		validation.Field(&input.Limit,
-			validation.Required),
+		validation.Field(&input.UserId, validation.Required),
+		validation.Field(&input.Page, validation.Required),
+		validation.Field(&input.PerPage, validation.Required),
 		validation.Field(&input.OrderBy, validation.Required),
 	)
 }
