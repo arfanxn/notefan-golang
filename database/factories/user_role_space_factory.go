12ed8ae3ad6a35b7a996bcdd949d720fe53e2a80
arfanxn <arfan2173@gmail.com>
2023-03-10 14:17:59 +0700

    make user role space factory

---
+++ database/factories/user_role_space_factory.go
@@ -0,0 +1,20 @@
+package factories
+
+import (
+	"time"
+
+	"github.com/notefan-golang/helpers/nullh"
+	"github.com/notefan-golang/models/entities"
+)
+
+func FakeUserRoleSpace() entities.UserRoleSpace {
+	urs := entities.UserRoleSpace{
+		// UserId : // will be filled in later
+		// RoleId : // will be filled in later
+		// SpaceId : // will be filled in later
+		CreatedAt: time.Now().AddDate(0, 0, -1),
+		UpdatedAt: nullh.RandSqlNullTime(time.Now().AddDate(0, 0, 1)),
+	}
+
+	return urs
+}
