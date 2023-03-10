c8537641487b2ac24e68bb543343e64b61992c06
arfanxn <arfan2173@gmail.com>
2023-03-11 01:42:45 +0700

    register space CRUD routes

---
+++ routes/space_routes.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	cc "github.com/notefan-golang/containers/controllers"
+)
+
+// registerSpaceRoutes registers routes for user module
+func registerSpaceRoutes(router *mux.Router, db *sql.DB) {
+	spaceController := cc.InitializeSpaceController(db)
+
+	// Space subrouters
+	usersSelf := router.PathPrefix("/users/self").Subrouter()
+	usersSelfSpaces := usersSelf.PathPrefix("/spaces").Subrouter()
+	spaces := router.PathPrefix("/spaces").Subrouter()
+
+	usersSelfSpaces.HandleFunc("", spaceController.Get).Methods(http.MethodGet)
+	spaces.HandleFunc("/{id}", spaceController.Find).Methods(http.MethodGet)
+	spaces.HandleFunc("", spaceController.Create).Methods(http.MethodPost)
+	spaces.HandleFunc("/{id}", spaceController.Update).Methods(http.MethodPut)
+}
