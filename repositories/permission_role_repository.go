c8e30e8a588875d6305318fc5d163c71546a1ca6
arfanxn <arfan2173@gmail.com>
2023-03-13 00:10:14 +0700

    add annotations

--- repositories/permission_role_repository.go
+++ repositories/permission_role_repository.go
@@ -22,22 +22,24 @@ func NewPermissionRoleRepository(db *sql.DB) *PermissionRoleRepository {
 	}
 }
 
+// Insert inserts into the database table
 func (repository *PermissionRoleRepository) Insert(
 	ctx context.Context, permissionRoles ...*entities.PermissionRole) (
 	sql.Result, error) {
 	query := buildBatchInsertQuery(repository.tableName, len(permissionRoles), repository.columnNames...)
-	valueArgs := []any{}
+	var valueArgs []any
 	for _, permissionRole := range permissionRoles {
 		valueArgs = append(valueArgs,
 			permissionRole.PermissionId, permissionRole.RoleId, permissionRole.CreatedAt)
 	}
-	result, err := repository.db.ExecContext(ctx, query, valueArgs)
+	result, err := repository.db.ExecContext(ctx, query, valueArgs...)
 	if err != nil {
 		return result, err
 	}
 	return result, nil
 }
 
+// Create do same as Insert but singularly
 func (repository *PermissionRoleRepository) Create(ctx context.Context, permissionRole *entities.PermissionRole) (
 	sql.Result, error) {
 	return repository.Insert(ctx, permissionRole)
