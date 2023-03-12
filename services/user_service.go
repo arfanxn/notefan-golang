f5125b8838fdfada2ca1985d135c98299cb7c803
arfanxn <arfan2173@gmail.com>
2023-03-12 20:11:07 +0700

    update user service error handling

--- services/user_service.go
+++ services/user_service.go
@@ -2,13 +2,12 @@ package services
 
 import (
 	"context"
-	"errors"
 	"path/filepath"
 	"sync"
 
+	"github.com/google/uuid"
 	media_collnames "github.com/notefan-golang/enums/media/collection_names"
 	"github.com/notefan-golang/exceptions"
-	"github.com/notefan-golang/helpers/errorh"
 	"github.com/notefan-golang/helpers/reflecth"
 	"github.com/notefan-golang/models/entities"
 	"github.com/notefan-golang/models/requests/user_reqs"
@@ -37,72 +36,85 @@ func NewUserService(
 }
 
 // Find Finds user by id and returns user with its relations
-func (service *UserService) Find(ctx context.Context, id string) (user_ress.User, error) {
-	user := user_ress.User{}
-
-	userEntity, err := service.repository.Find(ctx, id)
-	if err != nil { // err not nil return exception HTTPNotFound
-		errorh.Log(err)
-		return user, exceptions.HTTPNotFound
+func (service *UserService) Find(ctx context.Context, id string) (
+	userRes user_ress.User, err error) {
+	userEty, err := service.repository.Find(ctx, id)
+	if err != nil {
+		return
 	}
-	user = user_ress.FillFromEntity(userEntity)
+	if userEty.Id == uuid.Nil { // err not nil return exception HTTPNotFound
+		err = exceptions.HTTPNotFound
+		return
+	}
+	userRes = user_ress.FillFromEntity(userEty)
 
 	// Get User avatar
-	avatar, err := service.mediaRepository.FindByModelAndCollectionName(
+	avatarMediaEty, err := service.mediaRepository.FindByModelAndCollectionName(
 		ctx,
-		reflecth.GetTypeName(userEntity),
-		userEntity.Id.String(),
+		reflecth.GetTypeName(userEty),
+		userEty.Id.String(),
 		media_collnames.Avatar,
 	)
-	if errors.Is(err, exceptions.HTTPNotFound) { // if user doesn't have avatar return user without avatar
-		return user, nil
+	if avatarMediaEty.Id == uuid.Nil { // if user doesn't have avatar return user without avatar
+		return userRes, nil
 	}
 
-	user.Avatar = media_ress.FillFromEntity(avatar) // assign user avatar with avatar
-	return user, nil
+	userRes.Avatar = media_ress.FillFromEntity(avatarMediaEty) // assign user avatar with avatar
+	return userRes, nil
 }
 
 // UpdateProfile updates the given user update request
-func (service *UserService) UpdateProfile(ctx context.Context, data user_reqs.UpdateProfile) (user_ress.User, error) {
+func (service *UserService) UpdateProfile(ctx context.Context, data user_reqs.UpdateProfile) (
+	userRes user_ress.User, err error) {
 	var (
-		userEty  entities.User
-		mediaEty entities.Media
-		userRes  user_ress.User
-		err      error
+		userEty        entities.User
+		avatarMediaEty entities.Media
 	)
 
 	// Get User entity
 	userEty, err = service.repository.Find(ctx, data.Id)
-	errorh.LogPanic(err) // panic if not found
+	if err != nil {
+		return
+	}
+	if userEty.Id == uuid.Nil { // return exceptions.HTTPNotFound ig userEty not found
+		err = exceptions.HTTPNotFound
+		return
+	}
 	// Convert request to entity
 	userEty.Name = data.Name
 
 	// Update User Entity
 	_, err = service.repository.UpdateById(ctx, &userEty)
-	errorh.LogPanic(err) // panic and log the error if error at update
+	if err != nil {
+		return
+	}
 	userRes = user_ress.FillFromEntity(userEty)
 
 	// if avatar/file is present and provided save avatar
 	if data.Avatar != nil && data.Avatar.IsProvided() {
 		// Get Media entity (user's avatar) for update operation
-		mediaEty, err = service.mediaRepository.FindByModelAndCollectionName(
+		avatarMediaEty, err = service.mediaRepository.FindByModelAndCollectionName(
 			ctx,
 			reflecth.GetTypeName(userEty),
 			data.Id,
 			media_collnames.Avatar,
 		)
-		errorh.LogPanic(err) // panic if find error
+		if err != nil {
+			return
+		}
 
 		// Assign new file to the media file
-		mediaEty.FileName = filepath.Base(data.Avatar.Name)
-		mediaEty.File = data.Avatar
+		avatarMediaEty.FileName = filepath.Base(data.Avatar.Name)
+		avatarMediaEty.File = data.Avatar
 
 		// Do update
-		_, err = service.mediaRepository.UpdateById(ctx, &mediaEty)
-		errorh.LogPanic(err) // panic and log the error if error at update
+		_, err = service.mediaRepository.UpdateById(ctx, &avatarMediaEty)
+		if err != nil {
+			return
+		}
 
 		// Assign the media response to user avatar response
-		userRes.Avatar = media_ress.FillFromEntity(mediaEty)
+		userRes.Avatar = media_ress.FillFromEntity(avatarMediaEty)
 	}
 
 	return userRes, nil
