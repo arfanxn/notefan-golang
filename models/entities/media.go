597c93316341d15a85309af302f275b32fbdd9ed
arfanxn <arfan2173@gmail.com>
2023-03-13 01:37:22 +0700

    add annotations

--- models/entities/media.go
+++ models/entities/media.go
@@ -13,20 +13,32 @@ import (
 )
 
 type Media struct {
-	Id             uuid.UUID      `json:"id"`
-	ModelType      string         `json:"model_type"`
-	ModelId        uuid.UUID      `json:"model_id"`
-	CollectionName string         `json:"collection_name"`
-	Name           sql.NullString `json:"name"`
-	FileName       string         `json:"file_name"`
-	MimeType       string         `json:"mime_type"`
-	Disk           string         `json:"disk"`
+	// Id will be generated automatically, can be set manually if needed
+	Id uuid.UUID `json:"id"`
+	// ModelType must be specified
+	ModelType string `json:"model_type"`
+	// ModelId must be specified
+	ModelId uuid.UUID `json:"model_id"`
+	// CollectionName will be autofilled with default CollectionName if not specified
+	CollectionName string `json:"collection_name"`
+	// Name can be null if not specified
+	Name sql.NullString `json:"name"`
+	// FileName will be autofilled by random alphanumeric characters if not specified
+	FileName string `json:"file_name"`
+	// MimeType will be autofilled by guessing the file bytes if not specified
+	MimeType string `json:"mime_type"`
+	// Disk will be autofilled with default disk if not specified
+	Disk string `json:"disk"`
+	// ConversionDisk can be null if not set
 	ConversionDisk sql.NullString `json:"conversion_disk"`
-	Size           int64          `json:"size"`
-	CreatedAt      time.Time      `json:"created_at"`
-	UpdatedAt      sql.NullTime   `json:"updated_at"`
+	// if not set will be autofilled with by guessing the file bytes size
+	Size int64 `json:"size"`
+	// CreatedAt will be autofilled on creation
+	CreatedAt time.Time `json:"created_at"`
+	// UpdatedAt will be autofilled after updation
+	UpdatedAt sql.NullTime `json:"updated_at"`
 
-	// Metadata
+	// File Metadata, not in table columns
 	File *file_reqs.File `json:"-"`
 }
 
