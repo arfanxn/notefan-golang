cea64ce33444b416dd4f9031ef3fbc34cfe079a6
arfanxn <arfan2173@gmail.com>
2023-03-13 22:46:23 +0700

    make permission enums

---
+++ enums/permission/names/names.go
@@ -0,0 +1,54 @@
+package names
+
+// Notification Module Permissions
+const (
+	NotificationView   = "view notification"
+	NotificationDelete = "delete notification"
+)
+
+// Space Module Permissions
+const (
+	SpaceView   = "view space"
+	SpaceCreate = "create space"
+	SpaceUpdate = "update space"
+	SpaceDelete = "delete space"
+)
+
+// Space Member (User) Module Permissions
+const (
+	SpaceViewMember       = "view space member"
+	SpaceUpdateMemberRole = "update space member role"
+	SpaceDeleteMember     = "delete member"
+)
+
+// Page Module Permissions
+const (
+	PageView   = "view page"
+	PageCreate = "create page"
+	PageUpdate = "update page"
+	PageDelete = "delete page"
+)
+
+// Page Content  Module Permissions
+const (
+	PageContentView   = "view page content"
+	PageContentCreate = "create page content"
+	PageContentUpdate = "update page content"
+	PageContentDelete = "delete page content"
+)
+
+// Comment Module Permissions
+const (
+	CommentView   = "view comment"
+	CommentCreate = "create comment"
+	CommentUpdate = "update comment"
+	CommentDelete = "delete comment"
+)
+
+// Comment Reaction Module Permissions
+const (
+	CommentReactionView   = "view comment reaction"
+	CommentReactionCreate = "create comment reaction"
+	CommentReactionUpdate = "update comment reaction"
+	CommentReactionDelete = "delete comment reaction"
+)
