726866a6a4a3c8b44866130c2bf5a6177bb31470
arfanxn <arfan2173@gmail.com>
2023-03-14 00:53:34 +0700

    make FindByUserIdAndSpaceId

--- repositories/user_role_space_repository.go
+++ repositories/user_role_space_repository.go
@@ -1,10 +1,12 @@
 package repositories
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
 	"time"
 
+	"github.com/notefan-golang/helpers/stringh"
 	"github.com/notefan-golang/models/entities"
 	"github.com/notefan-golang/models/requests/query_reqs"
 )
@@ -21,6 +23,72 @@ func NewUserRoleSpaceRepository(db *sql.DB) *UserRoleSpaceRepository {
 		Query:  query_reqs.Default(),
 		entity: entities.UserRoleSpace{},
 	}
+}
+
+/*
+ * ----------------------------------------------------------------
+ * Repository utilty methods ⬇
+ * ----------------------------------------------------------------
+ */
+
+// scanRows scans rows of the database and returns it as structs, and returns error if any error has occurred.
+func (repository *UserRoleSpaceRepository) scanRows(rows *sql.Rows) (
+	urss []entities.UserRoleSpace, err error) {
+	for rows.Next() {
+		urs := entities.UserRoleSpace{}
+		err := rows.Scan(
+			&urs.UserId,
+			&urs.RoleId,
+			&urs.SpaceId,
+			&urs.CreatedAt,
+			&urs.UpdatedAt,
+		)
+		if err != nil {
+			return urss, err
+		}
+		urss = append(urss, urs)
+	}
+
+	return urss, nil
+}
+
+// scanRow scans only a row of the database and returns it as struct, and returns error if any error has occurred.
+func (repository *UserRoleSpaceRepository) scanRow(rows *sql.Rows) (urs entities.UserRoleSpace, err error) {
+	urss, err := repository.scanRows(rows)
+	if err != nil {
+		return
+	}
+	if len(urss) == 0 {
+		return urs, nil
+	}
+	urs, err = urss[0], nil
+	return
+}
+
+/*
+ * ----------------------------------------------------------------
+ * Repository query methods ⬇
+ * ----------------------------------------------------------------
+ */
+
+// FindByUserIdAndSpaceId finds row by user id and space id
+func (repository *UserRoleSpaceRepository) FindByUserIdAndSpaceId(
+	ctx context.Context, userId string, spaceId string) (
+	urs entities.UserRoleSpace, err error) {
+	queryBuf := bytes.NewBufferString("SELECT ")
+	queryBuf.WriteString(stringh.SliceColumnToStr(repository.entity.GetColumnNames()))
+	queryBuf.WriteString(" FROM ")
+	queryBuf.WriteString(repository.entity.GetTableName())
+	queryBuf.WriteString(" WHERE `user_id` = ? AND `space_id` = ?")
+	rows, err := repository.db.QueryContext(ctx, queryBuf.String(), userId, spaceId)
+	if err != nil {
+		return
+	}
+	urs, err = repository.scanRow(rows)
+	if err != nil {
+		return
+	}
+	return
 }
 
 func (repository *UserRoleSpaceRepository) Insert(ctx context.Context, userRoleSpaces ...*entities.UserRoleSpace) (
