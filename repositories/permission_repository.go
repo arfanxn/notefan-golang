d1dc578ceef58165dec0ab7110dbab4125d00801
arfanxn <arfan2173@gmail.com>
2023-03-14 10:12:22 +0700

    add get method

--- repositories/permission_repository.go
+++ repositories/permission_repository.go
@@ -1,10 +1,13 @@
 package repositories
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
 	"strings"
 
+	"github.com/notefan-golang/helpers/entityh"
+	"github.com/notefan-golang/helpers/stringh"
 	"github.com/notefan-golang/models/entities"
 	"github.com/notefan-golang/models/requests/query_reqs"
 
@@ -24,6 +27,12 @@ func NewPermissionRepository(db *sql.DB) *PermissionRepository {
 		entity: entities.Permission{},
 	}
 }
+
+/*
+ * ----------------------------------------------------------------
+ * Repository utilty methods ⬇
+ * ----------------------------------------------------------------
+ */
 
 // scanRows scans rows of the database and returns it as structs, and returns error if any error has occurred.
 func (repository *PermissionRepository) scanRows(rows *sql.Rows) (permissions []entities.Permission, err error) {
@@ -47,9 +56,50 @@ func (repository *PermissionRepository) scanRow(rows *sql.Rows) (entities.Permis
 	return permissions[0], nil
 }
 
+/*
+ * ----------------------------------------------------------------
+ * Repository query methods ⬇
+ * ----------------------------------------------------------------
+ */
+
+func (repository *PermissionRepository) GetByRoleId(ctx context.Context, roleId string) (
+	permissions []entities.Permission, err error) {
+	queryBuf := bytes.NewBufferString("SELECT ")
+	queryBuf.WriteString(stringh.SliceColumnToStr(repository.entity.GetColumnNames()))
+	queryBuf.WriteRune(',')
+	queryBuf.WriteString(stringh.SliceColumnToStr(entityh.GetColumnNames(entities.PermissionRole{})))
+	queryBuf.WriteString(" FROM ")
+	queryBuf.WriteString(repository.entity.GetTableName())
+	queryBuf.WriteString(" INNER JOIN ")
+	queryBuf.WriteString(entityh.GetTableName(entities.PermissionRole{}))
+	queryBuf.WriteString(" ON ")
+	queryBuf.WriteString(repository.entity.GetTableName() + ".`id`")
+	queryBuf.WriteString(" = ")
+	queryBuf.WriteString(entityh.GetTableName(entities.PermissionRole{}) + ".`permission_id`")
+	queryBuf.WriteString(" WHERE ")
+	queryBuf.WriteString(entityh.GetTableName(entities.PermissionRole{}) + "`role_id` = ?")
+
+	rows, err := repository.db.QueryContext(ctx, queryBuf.String(), roleId)
+	if err != nil {
+		return permissions, err
+	}
+	for rows.Next() {
+		var permission entities.Permission
+		var permissionRole entities.PermissionRole
+		err := rows.Scan(&permission.Id, &permission.Name,
+			&permissionRole.PermissionId, &permissionRole.RoleId, &permissionRole.CreatedAt)
+		if err != nil {
+			return permissions, err
+		}
+		permissions = append(permissions, permission)
+	}
+	return permissions, nil
+}
+
 func (repository *PermissionRepository) All(ctx context.Context) (
 	permissions []entities.Permission, err error) {
-	query := "SELECT id, name FROM " + repository.entity.GetTableName()
+	query := "SELECT " + strings.Join(repository.entity.GetColumnNames(), ", ") +
+		" FROM " + repository.entity.GetTableName()
 	rows, err := repository.db.QueryContext(ctx, query)
 	if err != nil {
 		return permissions, err
