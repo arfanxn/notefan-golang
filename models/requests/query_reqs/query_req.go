2738ed65a0dd8be7631207558081fb0b0480cde1
arfanxn <arfan2173@gmail.com>
2023-03-14 13:45:25 +0700

    update query req

--- models/requests/query_reqs/query_req.go
+++ models/requests/query_reqs/query_req.go
@@ -1,11 +1,13 @@
 package query_reqs
 
+import "github.com/notefan-golang/helpers/sliceh"
+
 // Query represents user update profile request
 type Query struct {
 	Keyword  string // saerch keyword
 	Limit    int
 	Offset   int64
-	Withs    map[string]bool
+	Withs    []string
 	Wheres   []map[string]any
 	OrWheres []map[string]any
 	OrderBys []map[string]string
@@ -16,4 +18,9 @@ func Default() Query {
 		Offset: 0,
 		Limit:  100,
 	}
+}
+
+// IsWith checks if the query.Withs contains the specified with
+func (query *Query) IsWith(with string) bool {
+	return sliceh.Contains(query.Withs, with)
 }
