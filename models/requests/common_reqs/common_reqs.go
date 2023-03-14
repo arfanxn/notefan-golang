9899a933a92cc286fc16f60c4efd72ed1b73cb5a
arfanxn <arfan2173@gmail.com>
2023-03-14 22:15:30 +0700

    add pagination support on common reqs

--- models/requests/common_reqs/common_reqs.go
+++ models/requests/common_reqs/common_reqs.go
@@ -14,3 +14,21 @@ func (input UUID) Validate() error {
 		validation.Field(&input.Id, validation.Required, ozzoIs.UUID),
 	)
 }
+
+type UUIDPagination struct {
+	Id      string `json:"id"`
+	Page    int64  `json:"page"`
+	PerPage int    `json:"per_page"`
+	OrderBy string `json:"order_by"`
+	Keyword string `json:"keyword"` // the search keyword
+}
+
+func (input UUIDPagination) Validate() error {
+	return validation.ValidateStruct(&input,
+		validation.Field(&input.Id, validation.Required, ozzoIs.UUID),
+		validation.Field(&input.Page, validation.Required),
+		validation.Field(&input.PerPage, validation.Required),
+		validation.Field(&input.OrderBy),
+		validation.Field(&input.Keyword),
+	)
+}
