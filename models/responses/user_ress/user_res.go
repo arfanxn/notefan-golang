3206d3427fcabd33d3ad03fb3b652e8f20b6d7d3
arfanxn <arfan2173@gmail.com>
2023-03-15 07:29:28 +0700

    update request

--- models/responses/user_ress/user_res.go
+++ models/responses/user_ress/user_res.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/notefan-golang/models/entities"
 	mediaRess "github.com/notefan-golang/models/responses/media_ress"
+	"github.com/notefan-golang/models/responses/role_ress"
 	"gopkg.in/guregu/null.v4"
 )
 
@@ -15,15 +16,20 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt null.Time `json:"updated_at"`
 
+	// Role represents user role
+	Role   role_ress.Role  `json:"role"`
 	Avatar mediaRess.Media `json:"avatar,omitempty"`
 }
 
 func FillFromEntity(entity entities.User) User {
-	return User{
+	res := User{
 		Id:        entity.Id.String(),
 		Name:      entity.Name,
 		Email:     entity.Email,
 		CreatedAt: entity.CreatedAt,
 		UpdatedAt: null.NewTime(entity.UpdatedAt.Time, entity.UpdatedAt.Valid),
+		Role:      role_ress.FillFromEntity(entity.Role),
 	}
+
+	return res
 }
