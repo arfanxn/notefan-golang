91d61ec9f379dc842f4cca950917418c899e6188
arfanxn <arfan2173@gmail.com>
2023-03-15 07:31:38 +0700

    update request

--- models/requests/space_member_reqs/get_req.go
+++ models/requests/space_member_reqs/get_req.go
@@ -6,18 +6,16 @@ import (
 )
 
 type Get struct {
-	SpaceId  string `json:"space_id"`
-	MemberId string `json:"member_id"`
-	Page     int64  `json:"page"`
-	PerPage  int    `json:"per_page"`
-	OrderBy  string `json:"order_by"`
-	Keyword  string `json:"keyword"` // the search keyword
+	SpaceId string `json:"space_id"`
+	Page    int64  `json:"page"`
+	PerPage int    `json:"per_page"`
+	OrderBy string `json:"order_by"`
+	Keyword string `json:"keyword"` // the search keyword
 }
 
 func (input Get) Validate() error {
 	return validation.ValidateStruct(&input,
 		validation.Field(&input.SpaceId, validation.Required, ozzoIs.UUID),
-		validation.Field(&input.MemberId, validation.Required, ozzoIs.UUID),
 		validation.Field(&input.Page, validation.Required),
 		validation.Field(&input.PerPage, validation.Required),
 		validation.Field(&input.OrderBy),
