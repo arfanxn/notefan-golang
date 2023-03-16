e2c7d53866702f0e20b40568aa3c198c00f979ce
arfanxn <arfan2173@gmail.com>
2023-03-16 15:59:00 +0700

    add context helper func

--- helpers/contexth/contexth.go
+++ helpers/contexth/contexth.go
@@ -2,13 +2,19 @@ package contexth
 
 import "context"
 
+// GetAuthUser is a helper function to get the current logged in user from the given context
+func GetAuthUser(ctx context.Context) (userMap map[string]any) {
+	userMap, ok := ctx.Value("user").(map[string]any)
+	if !ok {
+		return
+	}
+	return
+}
+
 // GetAuthUserId is a helper function to get the current logged in user id from the given context
 func GetAuthUserId(ctx context.Context) string {
-	user, ok := ctx.Value("user").(map[string]any)
-	if !ok {
-		return ""
-	}
-	id, ok := user["id"].(string)
+	userMap := GetAuthUser(ctx)
+	id, ok := userMap["id"].(string)
 	if !ok {
 		return ""
 	}
