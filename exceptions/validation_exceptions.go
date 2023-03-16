7da56387aae32ac9674cfb2e84d9f9b269a9fe1c
arfanxn <arfan2173@gmail.com>
2023-03-16 11:10:44 +0700

    make NewValidationError method

--- exceptions/validation_exceptions.go
+++ exceptions/validation_exceptions.go
@@ -1,6 +1,21 @@
 package exceptions
 
-import "errors"
+import (
+	"errors"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+// NewValidationError create a validation error, from the given key and message
+func NewValidationError(key string, message string) error {
+	values := map[string]any{}
+	values[key] = ""
+	validationErr := validation.Validate(values, validation.Map(
+		validation.Key(key, validation.Length(10, 10).Error(message)),
+	),
+	)
+	return validationErr
+}
 
 var (
 	// ValidationPasswordNotMatch
