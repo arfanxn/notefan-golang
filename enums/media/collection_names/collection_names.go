563cc41ddcd8b532bb1c8f3320431755fc8b34ff
arfanxn <arfan2173@gmail.com>
2023-03-17 19:56:13 +0700

    add enum

--- enums/media/collection_names/collection_names.go
+++ enums/media/collection_names/collection_names.go
@@ -1,7 +1,8 @@
 package collection_names
 
 const (
-	Avatar = "avatar"
-	Icon   = "icon"
-	Cover  = "cover"
+	Avatar       = "avatar"
+	Icon         = "icon"
+	Cover        = "cover"
+	ContentMedia = "content_media"
 )
