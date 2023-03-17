ee305426f9d15b031a31907a0c13282de999643c
arfanxn <arfan2173@gmail.com>
2023-03-17 23:19:14 +0700

    make page content type enums

---
+++ enums/page_content/types/types.go
@@ -0,0 +1,15 @@
+package types
+
+// Page Content Types
+const (
+	Text   = "text"
+	Table  = "table"
+	List   = "list"
+	Link   = "link"
+	Img    = "img" // img stands for image
+	Doc    = "doc" // doc stands for document
+	Script = "script"
+
+	Other     = "other"
+	OtherFile = "other_file" // OtherFile represents unknown file type
+)
