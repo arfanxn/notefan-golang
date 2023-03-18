e2de0d2566cb5e362bb3947a06e648e9c150cc9e
arfanxn <arfan2173@gmail.com>
2023-03-18 12:58:20 +0700

    map helper

---
+++ helpers/maph/maph.go
@@ -0,0 +1,14 @@
+package maph
+
+// Map mapping map of T
+func Map[KEY, KEY2 comparable, VAL, VAL2 any](
+	items map[KEY]VAL,
+	callback func(KEY, VAL) (KEY2, VAL2),
+) map[KEY2]VAL2 {
+	var resultItems map[KEY2]VAL2
+	for key, val := range items {
+		key2, val2 := callback(key, val)
+		resultItems[key2] = val2
+	}
+	return resultItems
+}
