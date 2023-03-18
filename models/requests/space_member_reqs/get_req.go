4b051c4f18bc0e40e38a055beb1b3cbd9e6e07cc
arfanxn <arfan2173@gmail.com>
2023-03-18 16:14:16 +0700

    update get req

--- models/requests/space_member_reqs/get_req.go
+++ models/requests/space_member_reqs/get_req.go
@@ -1,16 +1,19 @@
 package space_member_reqs
 
 import (
-	validation "github.com/go-ozzo/ozzo-validation"
 	ozzoIs "github.com/go-ozzo/ozzo-validation/is"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/notefan-golang/helpers/entityh"
+	"github.com/notefan-golang/models/entities"
+	"github.com/notefan-golang/rules/query_rules"
 )
 
 type Get struct {
-	SpaceId string `json:"space_id"`
-	Page    int64  `json:"page"`
-	PerPage int    `json:"per_page"`
-	OrderBy string `json:"order_by"`
-	Keyword string `json:"keyword"` // the search keyword
+	SpaceId  string   `json:"space_id"`
+	Page     int64    `json:"page"`
+	PerPage  int      `json:"per_page"`
+	OrderBys []string `json:"order_bys"`
+	Keyword  string   `json:"keyword"` // the search keyword
 }
 
 func (input Get) Validate() error {
@@ -18,7 +21,10 @@ func (input Get) Validate() error {
 		validation.Field(&input.SpaceId, validation.Required, ozzoIs.UUID),
 		validation.Field(&input.Page, validation.Required),
 		validation.Field(&input.PerPage, validation.Required),
-		validation.Field(&input.OrderBy),
+		validation.Field(&input.OrderBys,
+			validation.By(
+				query_rules.OrderBys(entityh.GetColumnNames(entities.User{})),
+			)),
 		validation.Field(&input.Keyword),
 	)
 }
