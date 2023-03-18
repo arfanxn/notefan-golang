b874c9205a9416971f68d7c38646b23b0f68a043
arfanxn <arfan2173@gmail.com>
2023-03-18 16:35:18 +0700

    update user service

--- tests/units/services/user_service_test.go
+++ tests/units/services/user_service_test.go
@@ -8,35 +8,4 @@ import (
 
 func TestUserService(t *testing.T) {
 	// TODO: Complete user service
-
-	/*
-		require := require.New(t)
-
-		app, appErr := singletons.GetApp()
-		require.Nil(appErr)
-		userRepository := repositories.NewUserRepository(app.DB)
-		mediaRepository := repositories.NewMediaRepository(app.DB)
-		userService := services.NewUserService(userRepository, mediaRepository)
-		ctx := context.Background()
-
-		// create testing user
-		user := factories.FakeUser()
-		_, createErr := userRepository.Create(ctx, &user)
-		require.Nil(createErr)
-
-		t.Run("UpdateProfile", func(t *testing.T) {
-			userReq := user_reqs.UpdateProfile{
-				Id:     user.Id.String(),
-				Name:   faker.Name(),
-				Avatar: file_reqs.FillFromBytes(factories.FakeImageBuffer().Bytes()),
-			}
-			userRes, err := userService.UpdateProfile(ctx, userReq)
-			require.Nil(err)
-			require.Equal(userReq.Id, userRes.Id)
-			require.Equal(userReq.Name, userRes.Name)
-			require.NotZero(userRes.Email)
-			require.NotZero(userRes.CreatedAt)
-			require.NotZero(userRes.Avatar.Size)
-		})
-	*/
 }
