feb3b3fd14fa9d73fc31b5d5f48faf4c9ef81008
arfanxn <arfan2173@gmail.com>
2023-03-18 13:10:16 +0700

    add func GetKeys on maph

--- helpers/maph/maph.go
+++ helpers/maph/maph.go
@@ -1,6 +1,6 @@
 package maph
 
-// Map mapping a map 
+// Map mapping a map
 func Map[KEY, KEY2 comparable, VAL, VAL2 any](
 	items map[KEY]VAL,
 	callback func(KEY, VAL) (KEY2, VAL2),
@@ -11,4 +11,13 @@ func Map[KEY, KEY2 comparable, VAL, VAL2 any](
 		resultItems[key2] = val2
 	}
 	return resultItems
+}
+
+// GetKeys returns the keys of map
+func GetKeys[KEY comparable, VAL any](items map[KEY]VAL) []KEY {
+	var keys []KEY
+	for key := range items {
+		keys = append(keys, key)
+	}
+	return keys
 }
