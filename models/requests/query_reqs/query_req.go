d71e3d71e61d550db54f8bf13e82a0374f812ce2
arfanxn <arfan2173@gmail.com>
2023-03-18 13:10:28 +0700

    update query req

--- models/requests/query_reqs/query_req.go
+++ models/requests/query_reqs/query_req.go
@@ -1,22 +1,28 @@
 package query_reqs
 
-import "github.com/notefan-golang/helpers/sliceh"
+import (
+	query_order_bys "github.com/notefan-golang/enums/query/order_bys"
+	"github.com/notefan-golang/helpers/sliceh"
+)
 
-// Query represents user update profile request
+// Query represents database query request
 type Query struct {
 	Keyword  string // saerch keyword
 	Limit    int
 	Offset   int64
 	Withs    []string
-	Wheres   []map[string]any
-	OrWheres []map[string]any
-	OrderBys []map[string]string
+	Wheres   map[string]any
+	OrWheres map[string]any
+	OrderBys map[string]string
 }
 
 func Default() Query {
 	return Query{
 		Offset: 0,
 		Limit:  100,
+		OrderBys: map[string]string{
+			"created_at": query_order_bys.Desc,
+		},
 	}
 }
 
@@ -28,4 +34,9 @@ func (query *Query) IsWith(with string) bool {
 // AddWith adds / appends to query.Withs
 func (query *Query) AddWith(with string) {
 	query.Withs = append(query.Withs, with)
+}
+
+// AddOrderBy adds / appends to query.OrderBys
+func (query *Query) AddOrderBy(key, value string) {
+	query.OrderBys[key] = value
 }
